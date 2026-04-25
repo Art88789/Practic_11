e92f2c047684ee811b89f85ad7f38094cd4402e3
Art88789 <151448725+Art88789@users.noreply.github.com>
2026-04-25 09:25:40 +0700

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,11 +1,23 @@
+def get_cleaned_words(text):
+    """
+    Принимает строку, удаляет из слов знаки препинания 
+    и возвращает список очищенных слов.
+    """
+    punctuation = ".,!?;:'-\"()[]{}«»"
+    words = []
+
+    for word in text.split():
+        clean_word = ""
+        for char in word:
+            if char not in punctuation:
+                clean_word += char
+        
+        if clean_word:
+            words.append(clean_word)
+            
+    return words
+
+
 sentence = input()
-punctuation = ".,!?;:'-\"()[]{}«»"
-words = []
-for word in sentence.split():
-    clean_word = ""
-    for char in word:
-        if char not in punctuation:
-            clean_word = clean_word + char
-    if clean_word != "":
-        words.append(clean_word)
-print(words)
+result = get_cleaned_words(sentence)
+print(result)
